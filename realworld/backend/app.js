96195877fb61e477dc5e002abfd283746bb19409
niazlindan <75419708+niazlindan@users.noreply.github.com>
2021-01-25 11:04:20 +0500

    Update app.js

--- realworld/backend/app.js
+++ realworld/backend/app.js
@@ -95,6 +95,6 @@ app.use(function(err, req, res, next) {
 });
 
 // finally, let's start our server...
-var server = app.listen( process.env.PORT || 3100, function(){
+var server = app.listen( process.env.PORT || 3100,'0.0.0.0', function(){
   console.log('Listening on port ' + server.address().port);
 });
